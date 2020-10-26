34f41024f9dfae29cd8c7ab328c63dc5e56a8b2f
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2020-10-26 14:12:22 +0600

    Updates

---
+++ precache-manifest.9d90ebc9593b3d62cdf9bb7b5042f687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0325d1803f0fae81258dcedddb590ee5",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "ae9ab32623e466543c77",
+    "url": "/react-admin/static/css/main.1b86150f.chunk.css"
+  },
+  {
+    "revision": "ac0e424419691fe2d7ba",
+    "url": "/react-admin/static/js/2.07be8b20.chunk.js"
+  },
+  {
+    "revision": "58d64a5bc3b06f2933294a2521985f38",
+    "url": "/react-admin/static/js/2.07be8b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9ab32623e466543c77",
+    "url": "/react-admin/static/js/main.ab6cf53f.chunk.js"
+  },
+  {
+    "revision": "273a0fa75b29e45daa8b",
+    "url": "/react-admin/static/js/runtime-main.01a66440.js"
+  }
+]);
